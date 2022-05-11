428b8f3196effe6e51a166e116361a20a23ede9c
Thing-han, Lim <han.lim@chelpis.com>
2022-05-11 11:50:34 +0800

    add tests for token uri

--- test/token-uri.ts
+++ test/token-uri.ts
@@ -1,21 +1,51 @@
 import { expect } from "chai";
 
+const tokenURIPrefix = "https:/example.com/";
 export default function () {
-  it("Set 1 token", async function () {
+  it("Set 1 token call", async function () {
     expect(
       await this.haveFunNFT
         .connect(this.chelpis)
-        .setTokenURI([[1, "https://example.com"]])
+        .setTokenURI([[1, tokenURIPrefix + 1]])
     );
   });
 
   it("Set 1 token", async function () {
     await this.haveFunNFT
       .connect(this.chelpis)
-      .setTokenURI([[1, "https://example.com"]]);
+      .setTokenURI([[1, tokenURIPrefix + 1]]);
 
     expect(await this.haveFunNFT.connect(this.chelpis).tokenURI(1)).to.be.equal(
-      "https://example.com"
+      tokenURIPrefix + 1
+    );
+  });
+
+  it("Set 34 tokens call", async function () {
+    expect(
+      await this.haveFunNFT
+        .connect(this.chelpis)
+        .setTokenURI(
+          Array.from({ length: 34 }, (_, i) => [
+            i + 1,
+            tokenURIPrefix + (i + 1),
+          ])
+        )
+    );
+  });
+
+  it("Set 34 tokens", async function () {
+    const uris = Array.from({ length: 34 }, (_, i) => [
+      i + 1,
+      tokenURIPrefix + (i + 1),
+    ]);
+    await this.haveFunNFT.connect(this.chelpis).setTokenURI(uris);
+
+    await Promise.all(
+      Array.from({ length: 34 }, (_, i) => i + 1).map(async (i) => {
+        expect(
+          await this.haveFunNFT.connect(this.chelpis).tokenURI(i)
+        ).to.be.equal(tokenURIPrefix + i);
+      })
     );
   });
 }
