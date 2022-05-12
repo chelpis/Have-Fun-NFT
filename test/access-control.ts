e80fbbfd42ea6b8cd03e71956771ba2c87822f47
Thing-han, Lim <han.lim@chelpis.com>
2022-05-12 22:53:05 +0800

    fix test message

--- test/access-control.ts
+++ test/access-control.ts
@@ -3,12 +3,12 @@ import { keccak256 } from "ethers/lib/utils";
 import { skipWeeks } from "./common";
 
 export default function () {
-  it("setMaxSupply reverted, call by target", async function () {
+  it("setMaxSupply succeeded, call by target", async function () {
     skipWeeks(52);
     expect(await this.haveFunNFT.connect(this.target).setMaxSupply());
   });
 
-  it("setMaxSupply succeeded, call by chelpis", async function () {
+  it("setMaxSupply reverted, call by chelpis", async function () {
     skipWeeks(52);
     await expect(this.haveFunNFT.connect(this.chelpis).setMaxSupply()).to.be
       .reverted;
